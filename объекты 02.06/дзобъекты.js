88bc773e7b4c938619fdc9ce97b3398b405351fe
AMamasydykova <amamasydykova@beeline.kg>
2022-08-17 15:11:59 +0600

    init hw objects 02.06.2022

---
+++ объекты 02.06/дзобъекты.js
@@ -0,0 +1,20 @@
+let userSalaries = {
+    Ella: 100,
+    Sofia: 200,
+    Elli: 490
+}
+alert(userSalaries.Ella);
+alert(userSalaries.Sofia);
+
+
+let sum = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+
+console.log(sum.a + sum.b + sum.c);
+
+let data = true;
+let number = data === true ? 3 : 5;
+alert(number);
